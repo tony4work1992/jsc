dd2f7ae00491258413a5a03f57fe32b27c82d12a
Nghianht <nghianht@smartosc.com>
2024-12-02 21:40:40 +0700

    [RenameColumns] - complete dev

--- app/src/main/java/smartosc/jsc/applications/App.java
+++ app/src/main/java/smartosc/jsc/applications/App.java
@@ -7,24 +7,37 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import smartosc.jsc.applications.etl.mo_add_columns.AddColumnsExecuter;
+import smartosc.jsc.applications.etl.mo_rename_columns.RenameColumnsExecuter;
 
 public class App {
 
     public static void main(String[] args) {
-        String dataset = "[{\"name\": \"AnNM1\", \"gender\": \"Male\"}]";
+        String dataset = "[{\"name\": \"Nghianht\", \"gender\": \"Male\"}]";
         // @TODO The params should be extracted from the request
-        String addColumns1 = "[{\"column\": \"email\", \"value\": \"annm1@smartosc.com\"}]";
-        String addColumns2 = "[{\"column\": \"age\", \"value\": \"11\"}]";
+        String addColumns1 = "[{\"column\": \"email\", \"value\": \"nghianht@smartosc.com\"}]";
+        String addColumns2 = "[{\"column\": \"age\", \"value\": \"28\"}]";
+        String renameColumns = "[{\"oldColumnName\": \"email1\", \"newColumnName\": \"emp_email\"}," +
+                "{\"oldColumnName\": \"age\", \"newColumnName\": \"emp_age\"}]";
         ObjectMapper mapper = new ObjectMapper();
         try {
             JsonNode jsonDataset = mapper.readTree(dataset);
 
+            //Add columns
             AddColumnsExecuter addColumnsExecuter = new AddColumnsExecuter();
-            // @TODO Factory pattern will be used to create the executer
-            JsonNode returnData1 = addColumnsExecuter.execute(addColumns1, jsonDataset);
-            JsonNode returnData2 = addColumnsExecuter.execute(addColumns2, returnData1);
-            String updatedJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(returnData2);
+            JsonNode returnData = addColumnsExecuter.execute(addColumns1, jsonDataset);
+            returnData = addColumnsExecuter.execute(addColumns2, returnData);
+            String updatedJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(returnData);
+            System.out.println("Result after add columns:");
             System.out.println(updatedJson);
+            System.out.println("-------------------------");
+
+            //Rename columns
+            RenameColumnsExecuter renameColumnsExecuter = new RenameColumnsExecuter();
+            JsonNode dataAfterRename = renameColumnsExecuter.execute(renameColumns, returnData);
+            String renameDataJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(dataAfterRename);
+            System.out.println("Result after rename columns:");
+            System.out.println(renameDataJson);
+            System.out.println("-------------------------");
         } catch (Exception e) {
             e.printStackTrace();
         }
