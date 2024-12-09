ec5d5ff2bd3865edf88e2973902cf7ee75c0ed46
anhln2 <anhln2@smartosc.com>
2024-12-09 16:51:23 +0700

    Init project

--- app/src/main/java/smartosc/jsc/applications/etl/modules/base/processor/CsvProcessorInterface.java
+++
@@ -1,9 +0,0 @@
-package smartosc.jsc.applications.etl.modules.base.processor;
-
-import smartosc.jsc.applications.etl.modules.base.DataSet;
-import smartosc.jsc.applications.etl.modules.base.instruction.AbstractInstruction;
-
-public interface CsvProcessorInterface {
-
-    public DataSet execute(AbstractInstruction instruction, DataSet dataSet);
-}
